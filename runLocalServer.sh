3fcd2471582d477ae997a86035d39ecaf1b9158e
Jack Hines <jack-hines@hotmail.co.uk>
2023-09-25 10:09:30 +1000

    run local changes

--- runLocalServer.sh
+++ runLocalServer.sh
@@ -3,28 +3,16 @@
 # Check if Jekyll is installed
 if ! command -v jekyll &> /dev/null
 then
-    # Update system
-    sudo apt-get update -y && sudo apt-get upgrade -y
-    
-    # Install Ruby
-    sudo apt-get install ruby-full build-essential zlib1g-dev -y
-    
-    # Set up Environment Variables for Ruby
+    sudo apt-get install ruby-full build-essential zlib1g-dev
     echo '# Install Ruby Gems to ~/gems' >> ~/.bashrc
     echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
     echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
     source ~/.bashrc
-    
-    # Install Jekyll and Bundler
-    gem install jekyll bundler --user-install
+    gem install jekyll bundler
 fi
 
-# Check if bundle is installed
-if ! command -v bundle &> /dev/null
-then
-    # If not installed, install bundle
-    gem install bundler --user-install
-fi
+# Install the gems from the Gemfile.
+bundle install
 
-# Serve your Jekyll site locally
+# Serve your Jekyll site locally.
 bundle exec jekyll serve
